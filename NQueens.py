2095c37fa14a7720816f9b217e275a658bb8aca4
Hassan <hassanalgoz@gmail.com>
2017-03-09 17:52:37 +0300

    Added comments about the problem in NQueens.py

--- NQueens.py
+++ NQueens.py
@@ -5,6 +5,15 @@ n = argv[1]
 
 rows = [] # Throw queens on the board
 
+# Pick a random assignment.
+# Randomly select a variable for update. Use min-Conflict measure to set the value of the selected variable.
+# Instead of building things step by step, we pick a complete assignment and fix it
+# States: 4 queens in 4 columns (4^4 = 256 states)
+# Operators: Move queen in column
+# Goal test: No attacks
+# Evaluation: c(n) = number of attacks
+
+
 
 def heuristic(x, y):
 	threats = 0
