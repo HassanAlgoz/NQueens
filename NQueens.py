eb3d4e8c9daadda2ad0377c10dd15b146314f8a0
Hassan <hassanalgoz@gmail.com>
2017-03-11 23:53:52 +0300

    added comments and adjusted output format

--- NQueens.py
+++ NQueens.py
@@ -1,8 +1,6 @@
 import sys
 import random
-print(sys.argv)
-
-
+import time
 
 # Pick a random assignment.
 # Randomly select a variable for update. Use min-Conflict measure to set the value of the selected variable.
@@ -12,8 +10,10 @@ print(sys.argv)
 # Goal test: No attacks
 # Evaluation: c(n) = number of attacks
 
+# Constants
+QUEEN = "Q"
+EMPTY = "#"
 
-# conflicts
 def conflicts(row, col):
 	numThreats = 0
 	# Column
@@ -45,6 +45,12 @@ def conflicts(row, col):
 
 
 def updateConflicts(row, col):
+	# Update conflicts on values who might be affected
+	# Affected Variables are variables who are either
+	# in the same column, or diagonals. So we update
+	# them. This way we don't have to check the whole
+	# board.
+
 	# Column
 	for i in range(n):
 		if board[i][col] == QUEEN and i != row:
@@ -75,18 +81,17 @@ def move(row, old, new):
 	row[old] = EMPTY
 	row[new] = QUEEN
 
-
+def printBoard():
+	for i in range(n):
+		print("".join(board[i]))
 
 
 if __name__ == "__main__":
-	# Constants
-	QUEEN = "Q"
-	EMPTY = "#"
-
 	# Get Input
 	n = int(sys.argv[1])
+	print("Solving for " +str(n)+ " Queens..")
 
-	# Initialize Board
+	# Initialize Board: random assignment to all variables (rows)
 	board = []
 	for i in range(n):
 		row = []
@@ -96,62 +101,62 @@ if __name__ == "__main__":
 		row[r] = QUEEN
 		board.append(row)
 
-	for i in range(n):
-		print("".join(board[i]))
+	timeStart = float(time.time())
 
-	
-	import datetime
-	timeStart = datetime.datetime.now()
-	# Initialize our conflicted rows storage
+	# Lookup table for minimum conflicts of variables (rows)
+	# Updated when necessary only, rather than having to check
+	# all queens. We only check affected variables. O(n) Space Complexity
 	rowConflicts = []
 	for i in range(n):
 		col = board[i].index(QUEEN)
 		rowConflicts.append(conflicts(i, col))
 
-	# Game Loop
-	counter = 0
-	while(True):
-		counter += 1
 
+	# -------------------------------------------------------------------
+	# Game Loop (one game)
+	numberOfMoves = 0
+	while(True):
+		numberOfMoves += 1
+
+		# Goal Test
 		if sum(rowConflicts) == 0:
-			for i in range(n):
-				print("".join(board[i]))	
-			print("GOAL!")
-			print("num of moves " + str(counter))
-			print("Elapsed Time", datetime.datetime.now() - timeStart)
+			# printBoard()
+			timeEnd = float(time.time())
+			timeDiff = timeEnd - timeStart
+			print "DONE!"
+			print "     \tN\tSeconds\tMoves"
+			print "     \t%d\t%.2f\t%d" % (n, timeDiff, numberOfMoves)
 			break
 
+		# Select a random variable (row)
 		r = random.randint(0, n-1)
 		# while(rowConflicts[r] == 0):
 		# 	r = random.randint(0, n-1)
-
+		# Assign it to minimum conflict column
 		myRow = board[r]
 		minConflict = rowConflicts[r]
 		initialIndex = board[r].index(QUEEN)
 		minConflictIndex = initialIndex
-
-
 		cellsConflicts = []
-
 		for j in range(n):
 			conflictResult = conflicts(r, j)
 			if (conflictResult < minConflict):
 				minConflict = conflictResult
 			cellsConflicts.append(conflictResult)
+		# Break ties randomly
 		while True:
 			rnd = random.randint(0,n-1)
 			if cellsConflicts[rnd] == minConflict:
 				minConflictIndex = rnd
 				break
 
-		# Move
+		# Move Queen: assign another value to the variable (row)
 		move(myRow, initialIndex, minConflictIndex)
 		rowConflicts[r] = minConflict
+		# Update conflicts on variables who have conflict with
+		# the position of this Queen before the move and after.
 		updateConflicts(r, initialIndex)
 		updateConflicts(r, minConflictIndex)
 
-	# 	for i in range(n):
-	# 		print("".join(board[i]))
+		# printBoard()
 	# print(rowConflicts)
-
-	
